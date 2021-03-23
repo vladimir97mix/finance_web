b71d6f056f7295f6719fcba4298ba4a86b3ae8d0
vladimir97mix <vladimir97mix@gmail.com>
2021-03-23 14:49:36 +0700

    Рабочая версия с отчетами

---
+++ main.py
@@ -0,0 +1,104 @@
+from flask import Flask, request, render_template, url_for, request, redirect
+from queries import queries
+
+app = Flask(__name__)
+
+
+@app.route('/costs', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    # Если метод формы POST
+    if request.method == 'POST':
+        # Забираем данные из формы
+        date = request.form['date']
+        products = request.form['text1']
+        productsH = request.form['text2']
+        clothes = request.form['text3']
+        health = request.form['text4']
+        debts = request.form['text5']
+        communication = request.form['text6']
+        transport = request.form['text7']
+        fun = request.form['text8']
+
+        costsTuple = (date, products, productsH, clothes, health, debts, communication, transport, fun)
+
+        # Вносим данные в таблицу
+        queries.pgInsertUpdateCosts(costsTuple)
+        return redirect('/')
+
+    # Если метод GET
+    else:
+        return render_template("index.html")
+
+
+@app.route('/income', methods=['POST', 'GET'])
+def income():
+    if request.method == 'POST':
+        date = request.form['date']
+        income1 = request.form['text1']
+        income2 = request.form['text2']
+
+        incomeTuple = (date, income1, income2)
+
+        queries.pgInsertUpdateIncome(incomeTuple)
+        return redirect('/income')
+    else:
+        return render_template('income.html')
+
+
+@app.route('/report', methods=['POST', 'GET'])
+def report():
+    if request.method == 'POST':
+        # Получение диапозона дат из формы
+        date_start = request.form['date_start']
+        date_end = request.form['date_end']
+        # Запрос в базу данных по диапозону дат
+        row = queries.pgSelectForReport(date_start, date_end)
+        # Подсчет итоговых значений
+        r1, r2, r3, r4, r5, r6, r7, r8 = 0, 0, 0, 0, 0, 0, 0, 0
+        total = 0.0
+        for el in row:
+            r1 += el[1]
+            r2 += el[2]
+            r3 += el[3]
+            r4 += el[4]
+            r5 += el[5]
+            r6 += el[6]
+            r7 += el[7]
+            r8 += el[8]
+            for elem in el[1:]:
+                total = total + elem
+        list_total = [str(round(r1,2)), str(round(r2,2)), str(round(r3,2)), str(round(r4,2)), str(round(r5,2)),
+                      str(round(r6,2)), str(round(r7,2)), str(round(r8,2)), str(round(total,2))]
+
+        # Вернуть результат на веб страницу
+        return render_template('report.html', row=row, date_start=date_start, date_end=date_end, list_total=list_total)
+    else:
+        return render_template('report.html')
+
+
+@app.route('/report2', methods=['POST', 'GET'])
+def report2():
+    if request.method == 'POST':
+        # Получение диапозона дат из формы
+        date_start = request.form['date_start']
+        date_end = request.form['date_end']
+        # Запрос в базу данных по диапозону дат
+        row = queries.pgSelectForReport2(date_start, date_end)
+        # Подсчет итоговых значений
+        r1, r2 = 0, 0
+        total = 0.0
+        for el in row:
+            r1 += el[1]
+            r2 += el[2]
+            for elem in el[1:]:
+                total = total + elem
+        list_total = [str(r1), str(r2), str(total)]
+        return render_template('report2.html', row=row, date_start=date_start, date_end=date_end, list_total=list_total)
+
+    else:
+        return render_template('report2.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="localhost")
---
+++ queries/queries.py
@@ -0,0 +1,77 @@
+import psycopg2
+from settings import dbstg
+
+# Настройки подключения к бд
+database = dbstg.stg['database']
+user = dbstg.stg['user']
+password = dbstg.stg['password']
+host = dbstg.stg['host']
+port = dbstg.stg['port']
+
+
+def pgSelectForReport(date_start, date_end):
+    db = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    cursor = db.cursor()
+    # Запрос по диапозону дат
+    cursor.execute(
+        """SELECT * FROM costs WHERE (Date BETWEEN '{0}' AND '{1}')""".format(str(date_start), str(date_end)))
+    row = cursor.fetchall()
+    return row
+
+
+def pgInsertUpdateCosts(values):
+    date = values[0]
+    db = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM costs WHERE date='%s'""" % str(date))
+    rows = cursor.fetchall()
+    # Если такой строки нет то, добавляем новую строку
+    if not rows:
+        cursor.execute('''INSERT INTO costs VALUES %s''' % str(values))
+        db.commit()
+        db.close()
+    # Если есть то выполняем сложение с существующей строкой
+    else:
+        cursor.execute("""UPDATE costs SET
+                products = products + {0},
+                productsh = productsh + {1},
+                clothes = clothes + {2},
+                health = health + {3},
+                debts = debts +{4},
+                communications = communications + {5},
+                transport = transport + {6},
+                fun = fun + {7}  WHERE date= '{8}'""".format(values[1], values[2], values[3], values[4],
+                                                             values[5], values[6], values[7], values[8], date))
+        db.commit()
+        db.close()
+
+
+def pgInsertUpdateIncome(values):
+    date = values[0]
+    db = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM income WHERE date='%s'""" % str(date))
+    rows = cursor.fetchall()
+    # Если такой строки нет то, добавляем новую строку
+    if not rows:
+        cursor.execute('''INSERT INTO income VALUES %s''' % str(values))
+        db.commit()
+        db.close()
+    # Если есть то выполняем сложение с существующей строкой
+    else:
+        cursor.execute("""UPDATE income SET
+                   incomeyana = incomeyana + {0},
+                   incomevova = incomevova + {1}
+                   WHERE date = '{2}'""".format(values[1], values[2], date))
+        db.commit()
+        db.close()
+
+
+def pgSelectForReport2(date_start, date_end):
+    db = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    cursor = db.cursor()
+    # Запрос по диапозону дат
+    cursor.execute(
+        """SELECT * FROM income WHERE (Date BETWEEN '{0}' AND '{1}')""".format(str(date_start), str(date_end)))
+    row = cursor.fetchall()
+    return row
